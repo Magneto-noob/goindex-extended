6217b8199f97fba269b7c9c06f8742ee861c0eaa
Magneto-noob <87361323+Magneto-noob@users.noreply.github.com>
2021-10-20 11:38:38 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6236,10 +6236,10 @@ function file_video(path) {
     .join("");
   player_items += `<li class="mdui-divider"></li>
                    <li class="mdui-menu-item"><a id="copy-link" class="mdui-ripple">Copy Link</a></li>`;
-  const playBtn = `
-      <button class="mdui-btn mdui-ripple mdui-color-theme-accent" mdui-menu="{target:'#player-items'}">
-        <i class="mdui-icon material-icons">&#xe039;</i>Play In External Player<i class="mdui-icon material-icons">&#xe5cf;</i>
-      </button>
+
+      
+        
+     
       <ul class="mdui-menu" id="player-items">${player_items}</ul>`;
   const content = `
 <div class="mdui-container-fluid">
